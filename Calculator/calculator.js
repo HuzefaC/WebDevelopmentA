c515cb4df286bcb1da908eb78ade5ceb3bbba7b4
HuzefaC <huzefachabukswar@gmail.com>
2021-07-01 22:04:43 +0530

    Add Calculator

---
+++ Calculator/calculator.js
@@ -0,0 +1,30 @@
+// jshint esversion:6
+
+const express = require("express");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+  let result = num1 + num2;
+  res.send(`The result is :${result}`);
+});
+
+app.get("/bmicalculator", (req, res) => {
+  res.sendFile(__dirname + "/bmicalculator.html");
+});
+
+app.post("/bmicalculator", (req, res) => {
+  let height = Number(req.body.height);
+  let weight = Number(req.body.weight);
+  let result = Number(weight / (height * height));
+  res.send(`The result is :${result}`);
+});
+
+app.listen(3000, () => console.log("Server started"));
