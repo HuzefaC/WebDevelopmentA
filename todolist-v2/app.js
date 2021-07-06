132ac0367ea700b3728a24616f4bbd902aa9e4c5
HuzefaC <huzefachabukswar@gmail.com>
2021-07-06 10:27:49 +0530

    Minor changes

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -1,20 +1,23 @@
 //jshint esversion:6
 
-const express = require("express");
-const mongoose = require("mongoose");
-const date = require(__dirname + "/date.js");
+const express = require('express');
+const mongoose = require('mongoose');
+const date = require(__dirname + '/date.js');
 
 const app = express();
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  'mongodb+srv://admin-huzefa:9021090952@cluster0.ulx0k.mongodb.net/todolistDB',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const itemsSchema = {
   name: {
@@ -23,19 +26,19 @@ const itemsSchema = {
   },
 };
 
-const Item = mongoose.model("Item", itemsSchema);
+const Item = mongoose.model('Item', itemsSchema);
 
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   const day = date.getDate();
   Item.find((err, results) => {
     if (err) {
     } else {
-      res.render("list", { listTitle: day, newListItems: results });
+      res.render('list', { listTitle: day, newListItems: results });
     }
   });
 });
 
-app.post("/", function (req, res) {
+app.post('/', function (req, res) {
   const item = req.body.newItem;
   const itemDB = new Item({
     name: item,
@@ -43,24 +46,24 @@ app.post("/", function (req, res) {
   itemDB.save((err) => {
     if (err) {
     }
-    res.redirect("/");
+    res.redirect('/');
   });
 });
 
-app.post("/delete", function (req, res) {
+app.post('/delete', function (req, res) {
   const checkedItemId = req.body.checkbox;
   Item.deleteOne({ _id: checkedItemId }, (err) => {
     if (err) {
     } else {
-      res.redirect("/");
+      res.redirect('/');
     }
   });
 });
 
-app.get("/about", function (req, res) {
-  res.render("about");
+app.get('/about', function (req, res) {
+  res.render('about');
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Server started');
 });
