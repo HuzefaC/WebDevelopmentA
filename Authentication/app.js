1d77dae570ba53e0290bf9a7f911610bd318bc5c
HuzefaC <huzefachabukswar@gmail.com>
2021-07-07 09:00:32 +0530

    Initial Commit

--- Authentication/app.js
+++ Authentication/app.js
@@ -1,1 +1,12 @@
 //jshint esversion:6
+
+const express = require('express');
+const ejs = require('ejs');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+  console.log('Sever running on port 3000');
+});
